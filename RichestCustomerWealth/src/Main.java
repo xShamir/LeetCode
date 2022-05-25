3541274afc5ebe7a8d031b013262278457fb8b68
Shamir Khan <xShamir37@gmail.com>
2022-05-25 22:03:50 +0300

    Update Main.java

--- RichestCustomerWealth/src/Main.java
+++ RichestCustomerWealth/src/Main.java
@@ -1,7 +1,22 @@
+import java.lang.reflect.Array;
+
 class Solution
 {
     public int maximumWealth(int[][] accounts)
     {
+        int max = 0;
+        int sum = 0;
 
+        for (int[] account : accounts) {
+            for (int j = 0; j < accounts[0].length; j++) {
+                sum += account[j];
+            }
+
+            if (sum > max) max = sum;
+
+            sum = 0;
+        }
+
+        return max;
     }
 }
